35dc014f6dc17dfe33bf94397a85efd3bf780dda
meto1336 <volkenandtd@gmail.com>
2022-11-06 10:44:02 +0100

    commit

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8bdc61bae8mshc7ff99c4061c91dp1c05f6jsn9d3fdc5000b5',
+		'X-RapidAPI-Host': 'reddit34.p.rapidapi.com'
+	}
+};
+
 let memes = {
 
     
     fetchMemes: function (){
-        fetch('https://meme-api.herokuapp.com/gimme/programmerhumor/9')
+        fetch('https://reddit34.p.rapidapi.com/getTopPostsBySubreddit?subreddit=arabfunny&time=hour', options)
 	    .then(response => response.json())
 	    .then((data) => this.displayMemes(data))
     },
     
     displayMemes: function(data){
 
-        for(let i = 0; i < 10; i++){
-            const {url} = data.memes[i]
-            document.querySelector('.img' + i).src = url
+        for(let i = 0; i < 9; i++){
+            //const {url} = data.memes[i]
+            //document.querySelector('.img' + i).style.background = url
+            
             
         }
+        console.log(data.data.posts[0])
+        console.log(data.data)
     }
 
 
