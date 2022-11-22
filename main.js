7a20e6b5c8a22b2f44fe125581f07dfd00d001ed
meto1336 <volkenandtd@gmail.com>
2022-11-22 23:04:08 +0100

    commit

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
 const options = {
 	method: 'GET',
 	headers: {
+		Authorization: '563492ad6f91700001000001f4b2e4998f8241b4912c8b3c306223b7',
 		'X-RapidAPI-Key': '8bdc61bae8mshc7ff99c4061c91dp1c05f6jsn9d3fdc5000b5',
-		'X-RapidAPI-Host': 'reddit34.p.rapidapi.com'
+		'X-RapidAPI-Host': 'PexelsdimasV1.p.rapidapi.com'
 	}
 };
 
-let  = {
-
-    
-    fetchMemes: function (){
-        fetch('https://pexelsdimasv1.p.rapidapi.com/v1/search?query=landscapes&locale=en-US&per_page=12&page=1', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    },
-    
-    displayMemes: function(data){
-
-        for(let i = 0; i <= 11; i++){
-            //const {url} = data.memes[i]
-            //document.querySelector('.img' + i).style.background = url
-            
-            
-        }
-        console.log(data.data.posts[0])
-        console.log(data.data)
-    }
+var forwardButton = document.getElementsByClassName('forward')
+var backwardsButton = document.getElementsByClassName('backwards')
 
 
+var page = 0;
+
+url = 'https:exelsdimasv1.p.rapidapi.com/v1/search?query=landscapes&locale=en-US&per_page=1&page=' 
+
+function increment() {
+
+    page++
+    url = url + page
+    console.log(url)
+    return url
 }
+
+console.log(new_url = increment())
+
+
+function fetchImages(){
+        fetch(new_url, options)
+        .then(response => response.json())
+        .then(data => this.displayImages(data))
+}
+
+    
+function displayImages(data){
+
+        for(let i = 0; i < 13; i++){
+            const image = data.photos[i].src.original
+            document.querySelector('.img' + i).src = image
+        }
+        
+}
+
+
+
+
 window.onload = (event) => {
-    memes.fetchMemes()
+    fetchImages()
 };
 
 window.location.reload = (event) => {
-    memes.fetchMemes()
+    fetchImages()
 };
